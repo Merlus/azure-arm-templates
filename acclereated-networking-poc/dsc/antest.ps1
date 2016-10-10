3633f27db45643aa2cc82ff6d2616e3061b9ce09
Michael <Michael@DABEAST>
2016-10-10 17:53:37 +1000

    Dsc to enable FW rules

---
+++ acclereated-networking-poc/dsc/antest.ps1
@@ -0,0 +1,35 @@
+﻿configuration antest
+{
+    Import-DscResource -ModuleName xNetworking -ModuleVersion 2.12.0.0
+
+    node localhost
+    {
+        xFirewall smb
+        {
+            Name = 'Allow SMB IN'
+            Enabled = 'True'
+            Direction = 'Inbound'
+            Action = 'Allow'
+            Profile = 'Domain,Private,Public'
+            Description = 'Allow SMB In'
+            Localport = '445'
+            Protocol = 'TCP'
+            RemoteAddress = 'Any'
+        }
+        xFirewall ping
+        {
+            Name = 'Allow Ping IN'
+            Enabled = 'True'
+            Direction = 'Inbound'
+            Action = 'Allow'
+            Profile = 'Domain,Private,Public'
+            Description = 'Allow Ping In'
+            IcmpType =  '0'
+        }
+    }
+
+}
+
+firewall -outputpath .\
+
+Start-DscConfiguration -Path .\ -Wait -Force
